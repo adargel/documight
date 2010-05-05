1257c00862bd2d93732cd0293073416e1eebb9cc
Aaron Dargel <aaron@nervousyak.com>
2010-05-05 18:05:41 -0500

    Removed redundant tests from ConsoleSpec because the argument defaults are now encapsulated in the Argument class.

--- src/Documight.Console.Specs/ConsoleSpecs.cs
+++ src/Documight.Console.Specs/ConsoleSpecs.cs
@@ -17,29 +17,20 @@ namespace Documight.Console.Specs
             };
     }
 
-    public class When_running_the_console_with_no_args : ConsoleSpecs
+    public class When_running_the_console
     {
-        Because of = () => console.Run();
+        protected static Console console;
+        protected static Mock<IDocumentation> documentEngine;
 
-        It creates_the_documentation_using_default_input_and_output_paths = () =>
+        Establish context = () =>
+        {
+            documentEngine = new Mock<IDocumentation>();
+            console = new Console(documentEngine.Object);
+        };
+
+        Because of = () => console.Run(new Arguments());
+
+        It calls_the_documentation_engine_with_the_parameters_given_to_generate_the_documentation = ()=>
             documentEngine.Verify(e => e.Create("document", "documentation"));
-    }
-
-    public class When_running_the_console_with_input_arg_and_no_output_arg : ConsoleSpecs
-    {
-        Because of = () =>
-            console.Run(new Arguments() { InputPath = "input" });
-
-        It uses_the_supplied_input_argument_and_default_output_argument = () =>
-            documentEngine.Verify(e => e.Create("input", "documentation"));
-    }
-
-    public class When_running_the_console_with_output_arg_and_no_input_arg : ConsoleSpecs
-    {
-        Because of = () =>
-            console.Run(new Arguments() { OutputPath = "output" });
-
-        It uses_the_supplied_input_argument_and_default_output_argument = () =>
-            documentEngine.Verify(e => e.Create("document", "output"));
     }
 }
