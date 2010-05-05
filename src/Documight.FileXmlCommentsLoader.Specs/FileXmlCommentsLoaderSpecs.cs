569d843de8390a953ef26268b2892d4d9cb74cea
Aaron Dargel <aaron@nervousyak.com>
2010-05-05 17:30:30 -0500

    Added no comments test to FileXmlCommentsLoaderSpecs.

--- src/Documight.FileXmlCommentsLoader.Specs/FileXmlCommentsLoaderSpecs.cs
+++ src/Documight.FileXmlCommentsLoader.Specs/FileXmlCommentsLoaderSpecs.cs
@@ -4,18 +4,33 @@ using Machine.Specifications;
 
 namespace Documight.FileXmlCommentsLoader.Specs
 {
-    public class When_loading_comments_from_a_directory_with_comment_files
+    public class FileXmlCommentsLoaderSpecs
     {
-        static FileXmlCommentsLoader loader;
-        static XmlComments[] comments;
+        protected static FileXmlCommentsLoader loader;
 
         Establish context = () => 
             loader = new FileXmlCommentsLoader();
+    }
+
+    public class When_loading_comments_from_a_directory_with_comment_files : FileXmlCommentsLoaderSpecs
+    {
+        static XmlComments[] comments;
 
         Because of = () => 
             comments = loader.LoadCommentsFrom(Environment.CurrentDirectory + @"\Fixtures\");
 
         It loads_the_comment_files_in_the_directory = () =>
             comments.Length.ShouldEqual(1);
+    }
+
+    public class When_loading_files_from_a_directory_without_comments : FileXmlCommentsLoaderSpecs
+    {
+        static XmlComments[] comments;
+
+        Because of = () =>
+            comments = loader.LoadCommentsFrom(Environment.CurrentDirectory + @"\..\");
+
+        It does_not_load_any_comments = () => 
+            comments.Length.ShouldEqual(0);
     }
 }
